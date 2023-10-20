71e38dfb4eb34a07b0d97ec9c3beea308640a494
Echosaii <67742281+BonitaWoralert@users.noreply.github.com>
2023-10-20 15:22:22 +0100

    poop slider and poop increase over time

---
+++ TP/Assets/Scripts/PoopBarUI.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PoopBarUI : MonoBehaviour
+{
+    [SerializeField] public Slider PoopBar;
+
+    private PlayerStatManager stats;
+    private PlayerUIManager ui;
+    private GameObject player;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        stats = player.GetComponent<PlayerStatManager>();
+        ui = player.GetComponent<PlayerUIManager>();
+        PoopBar.value = 0;
+        PoopBar.maxValue = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PoopBar.value < PoopBar.maxValue)
+        {
+            PoopBar.value = stats.GetPoop();
+        }
+    }
+    
+}
