e1b1248e34e43d42035485e61f55fd71785bcd69
Echosaii <67742281+BonitaWoralert@users.noreply.github.com>
2024-10-20 13:38:49 +0100

    Interactable door now sends player to new scene, toilet brazier is a prefab

--- TP/Assets/Scripts/Interact/Door.cs
+++ TP/Assets/Scripts/Interact/Door.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class Door : InteractableObject
 {
-    [SerializeField] private GameObject teleportLocation;
+    [SerializeField] private string sceneName;
     [SerializeField] private AudioSource openSource;
     [SerializeField] private AudioSource closedSource;
     [SerializeField] private AudioClip lockedLine;
@@ -14,52 +14,30 @@ public class Door : InteractableObject
     [SerializeField, Tooltip("If the door can be opened")] public bool locked = false;
 
     private Player player = null;
-    private IEnumerator delayedTask;
-
-    private void Start()
-    {
-    }
 
     override public bool Interact(Interact interact)
     {
-
-        //If theres a player
-        if (player != null)
+        if (player == null)
         {
-            //And the door Leads somewhere
-            if (teleportLocation != null && !locked)
-            {
-                player.ui.FadeThroughBlack();
-                openSource.Play();
-
-                //teleport the player to that somewhere
-                Teleport();
-
-                return true;
-            }
-            //If the door leads nowhere
-            else
-            {
-                closedSource.Play();
-                player.ui.DisplayMessage(lockedPrompt, 1.0f);
-                player.lines.PlaySound(lockedLine);
-                //It's locked
-                return false;            
-            }
+            return false;
         }
-        //The player was not close enough
-        return false;
-    }
+        if (sceneName == null || locked)
+        {
+            closedSource.Play();
+            player.ui.DisplayMessage(lockedPrompt, 1.0f);
+            player.lines.PlaySound(lockedLine);
+        }
 
-    private void Teleport()
-    {
-        player.transform.position = teleportLocation.transform.position;
+        player.ui.FadeThroughBlack();
+        openSource.Play();
+        SceneManager.LoadScene(sceneName);
+        return true;
     }
 
     public void OnTriggerEnter(Collider other)
     {
         //if a player enters the trigger
-        if (other.gameObject.name == "Player")
+        if (other.gameObject.layer == 7)
         {
             //Show it that it can interact
             player = other.GetComponentInParent<Player>();
@@ -70,7 +48,7 @@ public class Door : InteractableObject
     public void OnTriggerExit(Collider other)
     {
         //if a player exits the trigger
-        if (other.gameObject.name == "Player")
+        if (other.gameObject.layer == 7)
         {
             //Hide its interact message
             player = other.GetComponentInParent<Player>();
